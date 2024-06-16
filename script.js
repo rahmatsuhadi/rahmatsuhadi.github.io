8e7e88259e3686d249f1696d9cf8500f3f892e6e
Rahmat S <rahmat@carakan.id>
2024-06-16 18:33:57 +0700

    feat: add page new

---
+++ script.js
@@ -0,0 +1,21 @@
+var typed = new Typed('#typed', {
+    // strings: ['','Web Developer'],
+    strings: ['<i>Frontend Web Developer</i>.', 'Student at University'],
+    typeSpeed: 100,
+    loop:true,
+    fadeOut:true,
+    showCursor:false,
+})
+
+
+
+function scrollToSection() {
+    // Menggunakan smooth scrolling untuk mengarahkan ke elemen tertentu
+    document.querySelector('#form').scrollIntoView({ behavior: 'smooth' });
+}
+
+
+function scrollToSection() {
+    // Menggunakan smooth scrolling untuk mengarahkan ke elemen tertentu
+    document.querySelector('#form').scrollIntoView({ behavior: 'smooth' });
+}
